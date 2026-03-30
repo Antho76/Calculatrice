a2231d06dc5bdca5038c047c181730e8c2588d48
Antho <anthonin.pain@outlook.fr>
2026-03-30 13:46:41 +0200

    feat(button): first component

--- src/component/Button.vue
+++ src/component/Button.vue
@@ -5,25 +5,17 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
 
-import './button.css';
+import '../assets/main.css';
 
 const props = withDefaults(
   defineProps<{
-    /**
-     * The label of the button
-     */
+
     label: string;
-    /**
-     * primary or secondary button
-     */
+
     primary?: boolean;
-    /**
-     * size of the button
-     */
+
     size?: 'small' | 'medium' | 'large';
-    /**
-     * background color of the button
-     */
+
     backgroundColor?: string;
   }>(),
   { primary: false }
